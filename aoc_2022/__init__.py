b7d1ad47c1c97610d06cdeda74b6f0afe20c3121
David Pfahler <david@pfahler.at>
2022-12-09 08:11:58 +0100

    fixed formatting

--- aoc_2022/__init__.py
+++ aoc_2022/__init__.py
@@ -14,4 +14,4 @@ from aoc_2022.utils import load_lines
 from aoc_2022.aoc_2022 import start, usage
 
 __all__ = ['start', 'usage', 'load_lines', 'day_01', 'day_02', 'day_03',
-    'day_04', 'day_05', 'day_06', 'day_07', 'day_08', 'day_09', 'day_10']
+           'day_04', 'day_05', 'day_06', 'day_07', 'day_08', 'day_09', 'day_10']
