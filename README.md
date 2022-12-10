344500a29381cef17381cf0ec5283c60618af748
David Pfahler <david@pfahler.at>
2022-12-10 07:04:56 +0100

    solved day 10

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 [advent of code 2022](https://adventofcode.com/2022)
 
-![day](https://img.shields.io/badge/day%20📅-9-blue)
-![stars](https://img.shields.io/badge/stars%20⭐-18-yellow)
+![day](https://img.shields.io/badge/day%20📅-10-blue)
+![stars](https://img.shields.io/badge/stars%20⭐-20-yellow)
 ![GitHub CI](https://github.com/theShmoo/aoc2022/actions/workflows/workflow.yml/badge.svg)
 ![coverage](https://img.shields.io/badge/coverage-100%25-success)
 
@@ -24,13 +24,13 @@ or otherwise the pipeline will fail.
 ## Usage
 
 ```man
-usage: aoc_2022 [-h] [-d {1,2,3,4,5,6,7,8,9}] [-p {1,2}] [-t] [-e] [-r REPEAT] [-q]
+usage: aoc_2022 [-h] [-d {1,2,3,4,5,6,7,8,9,10}] [-p {1,2}] [-t] [-e] [-r REPEAT] [-q]
 
 advent of code 2022
 
 options:
   -h, --help            show this help message and exit
-  -d {1,2,3,4,5,6,7,8,9}, --day {1,2,3,4,5,6,7,8,9}
+  -d {1,2,3,4,5,6,7,8,9,10}, --day {1,2,3,4,5,6,7,8,9,10}
                         solve a specific day, if not set solve all days
   -p {1,2}, --part {1,2}
                         solve only for a specific part, if not set solve for all parts
--- aoc_2022/day_10.py
+++ aoc_2022/day_10.py
@@ -1,6 +1,51 @@
 """day 10 of aoc2022"""
 
 
+def addx(xs, x, next_x):
+    xs.append(next_x)
+    xs.append(next_x)
+    return next_x + x
+
+
+grid_w = 40
+grid_h = 6
+
+
+def in_sprite(x, current):
+    c_x = current % grid_w
+    return abs(c_x - x) <= 1
+
+
+def print_image(image):
+    s = "\n"
+    for row in image:
+        s += "".join(row) + '\n'
+    return s
+
+
 def day_10(part, data):
     """solve day 10 for the given part and data."""
-    return 0
+
+    next_x = 1
+    xs = []
+
+    for d in data:
+        if d == 'noop':
+            xs.append(next_x)
+        else:
+            s = d.split()
+            value = int(s[1])
+            next_x = addx(xs, value, next_x)
+
+    if part == 1:
+        to_sum = [20, 60, 100, 140, 180, 220]
+        return sum(xs[i-1] * i for i in to_sum)
+    else:
+        image = [['.'] * grid_w for _ in range(grid_h)]
+
+        for cycle, current in enumerate(xs):
+            x = cycle % grid_w
+            y = int(cycle / grid_w)
+            if in_sprite(x, current):
+                image[y][x] = '#'
+        return print_image(image)
